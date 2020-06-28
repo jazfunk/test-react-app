970ce19c5a3bbd8af3065462190c891e2508e188
Jeff King <jazfunk@gmail.com>
2020-06-27 20:27:46 -0400

    Two navbars displayed, testing between them

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Routes from "./routes/Routes";
-// import Navigation from "./Navigation";
+import Navigation from "./Navigation";
 import NavigationTwo from "./NavigationTwo";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Navigation />
         <NavigationTwo />
         <Routes />
       </div>
--- src/NavigationTwo.js
+++ src/NavigationTwo.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class NavigationTwo extends Component {
   render() {
     return (
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
         <a class="navbar-brand" href="#">
           Tweet Bridge
         </a>
